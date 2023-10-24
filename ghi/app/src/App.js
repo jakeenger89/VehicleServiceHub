f0aa945164aafe04f592096efebd65e6afe6ded7
sarah <sarcorkum@gmail.com>
2023-10-24 14:37:50 -0700

    closing tag fixed in App.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -32,22 +32,23 @@ function App() {
           <Route path="manufacturers">
             <Route path="" element={<ManufacturerList />} />
             <Route path="create" element={<ManufacturerForm />} />
-            <Route path="customers">
-              <Route path="" element={<CustomerList />} />
-              <Route path="add" element={<CustomerForm />} />
-            </Route>
-            <Route path="salespeople">
-              <Route path="" element={<SalesPersonList />} />
-              <Route path="add" element={<SalesPersonForm />} />
-            </Route>
-            <Route path="models">
-              <Route path="" element={<ModelList />} />
-              {/* <Route path="add" element={<SalesPersnForm/>} /> */}
-            </Route>
-            <Route path="automobiles">
-              <Route path="" element={<AutomobileList />} />
-              <Route path="create" element={<AutomobileForm />} />
-            </Route>
+          </Route>
+          <Route path="customers">
+            <Route path="" element={<CustomerList />} />
+            <Route path="add" element={<CustomerForm />} />
+          </Route>
+          <Route path="salespeople">
+            <Route path="" element={<SalesPersonList />} />
+            <Route path="add" element={<SalesPersonForm />} />
+          </Route>
+          <Route path="models">
+            <Route path="" element={<ModelList />} />
+            {/* <Route path="add" element={<SalesPersnForm/>} /> */}
+          </Route>
+          <Route path="automobiles">
+            <Route path="" element={<AutomobileList />} />
+            <Route path="create" element={<AutomobileForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
